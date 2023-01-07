b04c3841da589d9290bc5ed53e2d89c53d66dc4a
asirw <asirw@G15>
2023-01-07 12:12:28 +0530

    used lambda expression for logout mouse listener

--- src/bank/management/dash/DashboardHomeController.java
+++ src/bank/management/dash/DashboardHomeController.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -112,7 +113,7 @@ public class DashboardHomeController implements Initializable {
             Button logoutButton = new Button("Logout");
             logoutButton.setId("logoutButton");
             logoutButton.getStyleClass().add("logoutButton");
-            logoutButton.setOnMouseClicked(event2 -> {
+            logoutButton.setOnMouseClicked((MouseEvent event2) -> {
                 Alert alert = new Alert(AlertType.CONFIRMATION);
                 alert.setTitle("Confirm Exit");
                 alert.setHeaderText("Are you sure you want to exit?");
